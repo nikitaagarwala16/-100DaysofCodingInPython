2f8221160d35e6db9b72d3be1fee58c91f143025
nikitaagarwala16 <37476046+nikitaagarwala16@users.noreply.github.com>
2020-07-21 23:47:35 +0530

    Create 4sum.py

---
+++ 4sum.py
@@ -0,0 +1,33 @@
+def fourNumberSum(array, targetSum):
+  array.sort()
+  inputlen=len(array)
+  outputarray=[]
+  for i in range(0,inputlen):
+    for j in range(i+1,inputlen):
+      left=j+1
+      right=inputlen-1
+      while(left<right):
+        total=array[i]+array[j]+array[left]+array[right]
+        if(total==targetSum):
+          outputarray.append([array[right],array[left],array[j],array[i]])
+          left+=1
+          right-=1
+        elif total>targetSum :
+          right-=1
+        elif total<targetSum:
+          left+=1
+      while j+1<inputlen and array[j]==array[j+1] :
+        j+=1
+    while i+1<inputlen and array[i]==array[i+1] :
+        i+=1
+
+  return outputarray
+
+  
+  
+if __name__ == "__main__":
+  array = [7,6,4,-1,1,2]
+  targetSum = 16
+  output = fourNumberSum(array, targetSum)
+  print(output)
+  
